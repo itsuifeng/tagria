7d095450042de5020f606a2f6ec73dc2876be0c0
jslsolucoes <jonatan@jslsolucoes.com>
2016-12-07 01:58:53 -0200

    Including examples projects

---
+++ tagria-example-core/src/main/java/dao/PersonDao.java
@@ -0,0 +1,46 @@
+package dao;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import model.Person;
+
+public class PersonDao {
+
+	private static PersonDao instance;
+	private List<Person> persons = new ArrayList<Person>();
+
+	private PersonDao() {
+
+	}
+
+	public static PersonDao getInstance() {
+		if (instance == null)
+			instance = new PersonDao();
+		return instance;
+	}
+
+	public List<Person> list() {
+		return this.persons;
+	}
+
+	public Person load(Person person) {
+		return persons.get(persons.indexOf(person));
+	}
+
+	public void saveOrUpdate(Person person) {
+		if (person.getId() == null) {
+			person.setId(new Long(persons.size()));
+			persons.add(person);
+		} else {
+			Person load = load(person);
+			load.setName(person.getName());
+			load.setAge(person.getAge());
+		}
+	}
+
+	public void delete(Person produto) {
+		persons.remove(produto);
+	}
+	
+}
