d760d7f61c816d70243824775bbb9936ccca24f8
jonatan <jonatan@jslsolucoes.com>
2017-02-21 00:58:52 -0300

    Sonar improvs

--- tagria-example-servlet/src/main/java/servlet/AppServlet.java
+++ tagria-example-servlet/src/main/java/servlet/AppServlet.java
@@ -13,6 +13,7 @@ import javax.servlet.http.HttpServletResponse;
 public class AppServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 	
+	@Override
 	protected void doGet(HttpServletRequest request,
 			HttpServletResponse response) throws ServletException, IOException {
 		RequestDispatcher dispatcher = getServletContext()
--- tagria-example-servlet/src/main/java/servlet/PersonServlet.java
+++ tagria-example-servlet/src/main/java/servlet/PersonServlet.java
@@ -10,54 +10,60 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.apache.commons.lang.StringUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import dao.PersonDao;
 import model.Person;
 
-@WebServlet(name="person",urlPatterns="/person/*")
+@WebServlet(name = "person", urlPatterns = "/person/*")
 public class PersonServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-	private PersonDao personDao;
+	private static PersonDao personDao;
+	private static Logger logger  = LoggerFactory.getLogger(PersonServlet.class);
 
 	public PersonServlet() {
 		super();
 		personDao = PersonDao.getInstance();
 	}
 
-	protected void doGet(HttpServletRequest request,
-			HttpServletResponse response) throws ServletException, IOException {
-		String[] tokens = request.getRequestURI().split("/");
-		String url = tokens[tokens.length - 1];
-		if (url.equals("list")) {
-			request.setAttribute("personList", personDao.list());
-			RequestDispatcher dispatcher = getServletContext()
-					.getRequestDispatcher("/WEB-INF/jsp/person/list.jsp");
-			dispatcher.forward(request, response);
-		} else if (url.equals("form")) {
-			RequestDispatcher dispatcher = getServletContext()
-					.getRequestDispatcher("/WEB-INF/jsp/person/form.jsp");
-			dispatcher.forward(request, response);
-		} else if (url.equals("delete")) {
-			personDao.delete(new Person(Long.valueOf(request.getParameter("id"))));
-			response.sendRedirect(getServletContext() + "/person/list");
-		} else if (url.equals("edit")) {
-			request.setAttribute("person",
-					personDao.load(new Person(Long.valueOf(request.getParameter("id")))));
-			RequestDispatcher dispatcher = getServletContext()
-					.getRequestDispatcher("/WEB-INF/jsp/person/form.jsp");
-			dispatcher.forward(request, response);
+	@Override
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		try {
+			String[] tokens = request.getRequestURI().split("/");
+			String url = tokens[tokens.length - 1];
+			if ("list".equals(url)) {
+				request.setAttribute("personList", personDao.list());
+				RequestDispatcher dispatcher = getServletContext().getRequestDispatcher("/WEB-INF/jsp/person/list.jsp");
+				dispatcher.forward(request, response);
+			} else if ("form".equals(url)) {
+				RequestDispatcher dispatcher = getServletContext().getRequestDispatcher("/WEB-INF/jsp/person/form.jsp");
+				dispatcher.forward(request, response);
+			} else if ("delete".equals(url)) {
+				personDao.delete(new Person(Long.valueOf(request.getParameter("id"))));
+				response.sendRedirect(getServletContext() + "/person/list");
+			} else if ("edit".equals(url)) {
+				request.setAttribute("person", personDao.load(new Person(Long.valueOf(request.getParameter("id")))));
+				RequestDispatcher dispatcher = getServletContext().getRequestDispatcher("/WEB-INF/jsp/person/form.jsp");
+				dispatcher.forward(request, response);
+			}
+		} catch (IOException|ServletException exception) {
+			logger.error("could not execute get logic ",exception);
 		}
 	}
 
-	protected void doPost(HttpServletRequest request,
-			HttpServletResponse response) throws ServletException, IOException {
-		Long id = (!StringUtils.isEmpty(request.getParameter("id")) ? new Long(
-				request.getParameter("id")) : null);
-		String name = request.getParameter("name");
-		Integer age = Integer.valueOf(request.getParameter("age"));
-		personDao.saveOrUpdate(new Person(id, name, age));
-		response.sendRedirect(getServletContext().getContextPath()
-				+ "/person/list");
+	@Override
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		try {
+			Long id = (!StringUtils.isEmpty(request.getParameter("id")) ? new Long(request.getParameter("id")) : null);
+			String name = request.getParameter("name");
+			Integer age = Integer.valueOf(request.getParameter("age"));
+			personDao.saveOrUpdate(new Person(id, name, age));
+			response.sendRedirect(getServletContext().getContextPath() + "/person/list");
+		} catch (IOException exception) {
+			logger.error("Could not save person",exception);
+		}
 	}
 
 }
