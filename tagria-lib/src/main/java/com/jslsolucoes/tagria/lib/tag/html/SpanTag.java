636b3b7b27c631aff77b17d3a3916c36e34a6a88
jlemes <jonatan@jslsolucoes.com>
2018-01-25 00:00:27 -0200

    add label and rendered support to span

--- tagria-lib/src/main/java/com/jslsolucoes/tagria/lib/tag/html/SpanTag.java
+++ tagria-lib/src/main/java/com/jslsolucoes/tagria/lib/tag/html/SpanTag.java
@@ -20,6 +20,8 @@ import java.io.IOException;
 import javax.servlet.jsp.JspException;
 import javax.servlet.jsp.tagext.SimpleTagSupport;
 
+import org.apache.commons.lang.StringUtils;
+
 import com.jslsolucoes.tagria.lib.html.Attribute;
 import com.jslsolucoes.tagria.lib.html.Span;
 import com.jslsolucoes.tagria.lib.util.TagUtil;
@@ -27,14 +29,22 @@ import com.jslsolucoes.tagria.lib.util.TagUtil;
 public class SpanTag extends SimpleTagSupport {
 	
 	private String id;
+	private String label;
 	private String name;
+	private Boolean rendered = Boolean.TRUE;
 	
 	@Override
 	public void doTag() throws JspException, IOException {
-		Span span = new Span();
-		span.add(Attribute.ID,TagUtil.getId(name,id));
-		span.add(TagUtil.getBody(getJspBody()));
-		TagUtil.out(getJspContext(), span);
+		if(rendered != null && rendered){
+			Span span = new Span();
+			span.add(Attribute.ID,TagUtil.getId(name,id));
+			if (!StringUtils.isEmpty(label)) {
+				span.add(TagUtil.getLocalized(label,getJspContext()));
+			} else {
+				span.add(TagUtil.getBody(getJspBody()));
+			}
+			TagUtil.out(getJspContext(), span);
+		}
 	}
 
 	public String getId() {
@@ -51,6 +61,22 @@ public class SpanTag extends SimpleTagSupport {
 
 	public void setName(String name) {
 		this.name = name;
+	}
+
+	public Boolean getRendered() {
+		return rendered;
+	}
+
+	public void setRendered(Boolean rendered) {
+		this.rendered = rendered;
+	}
+
+	public String getLabel() {
+		return label;
+	}
+
+	public void setLabel(String label) {
+		this.label = label;
 	}
 
 }
