e0db9b680e96cefeaa477edf0ce6f2a4ab4e1234
jlemes <jonatan@jslsolucoes.com>
2018-02-15 20:35:22 -0200

    update version

--- tagria-lib/src/main/java/com/jslsolucoes/tagria/lib/util/TagUtil.java
+++ tagria-lib/src/main/java/com/jslsolucoes/tagria/lib/util/TagUtil.java
@@ -41,7 +41,7 @@ import com.jslsolucoes.tagria.lib.tag.html.DetailTableTag;
 
 public class TagUtil {
 
-	public static final String VERSION = "1.0.15.6";
+	public static final String VERSION = "1.0.16";
 	private static Logger logger = LoggerFactory.getLogger(TagUtil.class);
 
 	private TagUtil() {
