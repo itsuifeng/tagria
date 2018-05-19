e09bd10a1e9694a977b92edaade7db1b79d01cca
jlemes <jonatan@jslsolucoes.com>
2018-05-18 21:37:13 -0300

    Support more viewport and css class

--- tagria-lib/src/main/java/com/jslsolucoes/tagria/lib/tag/html/ColTag.java
+++ tagria-lib/src/main/java/com/jslsolucoes/tagria/lib/tag/html/ColTag.java
@@ -6,28 +6,83 @@ import java.io.IOException;
 import javax.servlet.jsp.JspException;
 import javax.servlet.jsp.tagext.SimpleTagSupport;
 
+import org.apache.commons.lang.StringUtils;
+
 import com.jslsolucoes.tagria.lib.html.Attribute;
 import com.jslsolucoes.tagria.lib.html.Div;
 import com.jslsolucoes.tagria.lib.util.TagUtil;
 
 public class ColTag extends SimpleTagSupport {
 
-	private Integer size;
+	private Integer extraSmall;
+	private Integer small;
+	private Integer medium;
+	private Integer large;
+	private String cssClass;
 
 	@Override
 	public void doTag() throws JspException, IOException {
 		Div div = new Div();
-		div.add(Attribute.CLASS, "col-xs-" + size);
+		if(extraSmall != null){
+			div.add(Attribute.CLASS, "col-xs-" + extraSmall);
+		}
+		if(small != null){
+			div.add(Attribute.CLASS, "col-sm-" + small);
+		}
+		if(medium != null){
+			div.add(Attribute.CLASS, "col-md-" + medium);
+		}
+		if(large != null){
+			div.add(Attribute.CLASS, "col-lg-" + large);
+		}
+		
+		if (!StringUtils.isEmpty(cssClass)) {
+			div.add(Attribute.CLASS, cssClass);
+		}
+
 		div.add(TagUtil.getBody(getJspBody()));
 		TagUtil.out(getJspContext(), div);
+		
 	}
 
-	public Integer getSize() {
-		return size;
+	public Integer getExtraSmall() {
+		return extraSmall;
 	}
 
-	public void setSize(Integer size) {
-		this.size = size;
+	public void setExtraSmall(Integer extraSmall) {
+		this.extraSmall = extraSmall;
+	}
+
+	public Integer getSmall() {
+		return small;
+	}
+
+	public void setSmall(Integer small) {
+		this.small = small;
+	}
+
+	public Integer getMedium() {
+		return medium;
+	}
+
+	public void setMedium(Integer medium) {
+		this.medium = medium;
+	}
+
+	public Integer getLarge() {
+		return large;
+	}
+
+	public void setLarge(Integer large) {
+		this.large = large;
+	}
+
+	public String getCssClass() {
+		return cssClass;
+	}
+
+	public void setCssClass(String cssClass) {
+		this.cssClass = cssClass;
 	}
 
 }
